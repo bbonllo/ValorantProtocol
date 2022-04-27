e351ee2d39a5234e1c670241688dbf6efe3f22f2
1dami <1dami@TartangaLH.eus>
2022-04-27 09:52:35 +0200

    Weapons

---
+++ model/WeaponManager.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public interface WeaponManager {
+	public void addWeapon(Weapon weapon);
+	public Weapon getWeaponByName(String name);
+	public boolean modifyWeapon(Weapon weapon);
+	public void deleteWeapon(String name);
+	public Set<Weapon> getAllWeapon();
+	public Set<String> getAllSidearms();
+	public Set<Weapon> getAllPrimary();
+}
