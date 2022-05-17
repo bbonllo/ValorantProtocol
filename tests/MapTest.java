5c546cf654cf3a44f8945335f1bfec59d7496075
1dami <1dami@TartangaLH.eus>
2022-05-17 13:00:25 +0200

    Testeos terminadooos

--- tests/MapTest.java
+++ tests/MapTest.java
@@ -9,19 +9,60 @@ import controlador.MapManagerDBImplementation;
 import exceptions.ExceptionManager;
 
 public class MapTest {
-	String name = "Split";
-	MapManager newMap = new MapManagerDBImplementation();
 
 	@Test
 	public void testGetMapByName() throws ExceptionManager {
+		String name = "Split";
+		MapManager newMap = new MapManagerDBImplementation();
 		assertNotNull(newMap.getMapByName(name));
 	}
 
 	@Test
-	public void testMakeStadisctic() {
+	public void testGetAllMaps() throws ExceptionManager {
+		MapManager newMap = new MapManagerDBImplementation();
+		assertNotNull(newMap.getAllMaps());
 	}
 
 	@Test
-	public void testGetAllMaps() {
+	public void testGetAttackMissionAgents() throws ExceptionManager {
+		String name = "Split";
+		MapManager newMap = new MapManagerDBImplementation();
+		assertNotNull(newMap.getAttackMissionAgents(name));
 	}
+
+	@Test
+	public void testGetDefendMissionAgents() throws ExceptionManager {
+		String name = "Split";
+		MapManager newMap = new MapManagerDBImplementation();
+		assertNotNull(newMap.getDefendMissionAgents(name));
+	}
+
+	@Test
+	public void testGetAttackMissionWeapon() throws ExceptionManager {
+		String name = "Split";
+		MapManager newMap = new MapManagerDBImplementation();
+		assertNotNull(newMap.getAttackMissionWeapon(name));
+	}
+
+	@Test
+	public void testGetDefendMissionWeapon() throws ExceptionManager {
+		String name = "Split";
+		MapManager newMap = new MapManagerDBImplementation();
+		assertNotNull(newMap.getDefendMissionWeapon(name));
+	}
+
+	@Test
+	public void testAgentPercentageMapAttack() throws ExceptionManager {
+		String name = "Split";
+		MapManager newMap = new MapManagerDBImplementation();
+		assertNotNull(newMap.agentPercentageMapAttack(name));
+	}
+
+	@Test
+	public void testAgentPercentageMapDefend() throws ExceptionManager {
+		String name = "Split";
+		MapManager newMap = new MapManagerDBImplementation();
+		assertNotNull(newMap.agentPercentageMapDefend(name));
+	}
+
 }
